0c583115e771e986bfebdcbfbe8b45d8a9a42c13
Prabhat mishra <105411899+Prabhatm09@users.noreply.github.com>
2022-10-05 13:43:01 +0530

    hooks

---
+++ hooks/src/App.js
@@ -0,0 +1,11 @@
+// import { CountClass } from "./components/Counter";
+import { CountHook } from "./components/CountHook";
+
+export function  App (){
+  return (
+    <>
+    {/* <CountClass /> */}
+    <CountHook />
+    </>
+  )
+}
