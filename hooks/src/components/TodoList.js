5141aee5ad997e6914ba80825bb395c2b8121a46
Prabhat mishra <105411899+Prabhatm09@users.noreply.github.com>
2022-10-12 23:13:53 +0530

    update

--- hooks/src/components/TodoList.js
+++ hooks/src/components/TodoList.js
@@ -1,28 +1,33 @@
-import React , { useState } from "react";
+import React, { useState } from "react";
 
-export function TodoList (){
-    const  [ items, setItems] = useState([]);
-    const [ lists , setList] = useState();
+export function TodoList() {
+    const [items, setItems] = useState([]);
+    const [lists, setList] = useState();
 
     const addItem = () => {
-        setItems([...items , lists]);
+        setItems([...items, lists]);
         setList("")
     }
 
     return (
         <>
-        <input type="text"  value={lists} onChange={(e) => setList(e.target.value)} /> 
-        <button onClick={addItem}>add</button>
-        {items.map((item)=> (
-            <div>
-            <span>{item}</span>
-            <button onClick={()=> {
-                let filteredarray = items.filter((value) => value !== item);
-                setItems([...filteredarray])
-            }}
-            >delete</button>
-            </div>
-        ))}
+            <input type="text" value={lists} onChange={(e) => setList(e.target.value)} />
+            <button onClick={addItem}>add</button>
+            {items.map((item) => (
+                <div>
+                    <span>{item}</span>
+                    <button onClick={() => {
+                        let filteredarray = items.filter((value) => value !== item);
+                        setItems([...filteredarray])
+                    }}
+                    >delete</button>
+                    <button onClick={() => {
+                        let filteredarray = items.filter((value) => value !== item);
+                        setItems([...filteredarray])
+                    }}
+                    >save</button>
+                </div>
+            ))}
         </>
     )
 }
