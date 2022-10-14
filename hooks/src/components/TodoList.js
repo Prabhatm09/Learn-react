3a7fdad975d3b8b6e589bd75f3bc3bbe7993dc76
Prabhat mishra <105411899+Prabhatm09@users.noreply.github.com>
2022-10-15 01:55:21 +0530

    update todo

--- hooks/src/components/TodoList.js
+++ hooks/src/components/TodoList.js
@@ -2,68 +2,90 @@ import React , { useState } from "react";
 
 export function TodoList () {
   
-    const [items , setItems] = useState([]);
-    const [lists , setLists] = useState();
+    const [inputTask , setInputTask] = useState({});
+    const [tasks , setTasks] = useState([]);
+    const [updateText , setUpdateText] = useState("")
 
     const addItem = () => {
-setItems([...items , lists])
-setLists("")
+setTasks([...tasks , inputTask])
+setInputTask({text : "" })
+    }
+
+    const makeEditable = (thisTask) => {
+      
+      // console.log(thisTask);
+      console.log("before", tasks);
+      let newTasks = [];
+      tasks.map((task) =>  {
+        let editable = false;
+        if(task.id === thisTask.id){
+          console.log("inside")
+          editable = true
+        }
+        newTasks.push({
+          ...task,
+          isEditable: editable
+        })
+      })
+      console.log("after update" , newTasks);
+      setTasks(newTasks)
+    }
+    
+
+    const updatetask = (thisTask) => {
+let newTasks = [];
+      tasks.map((task) =>  {
+        let text = task.text;
+        if(task.id === thisTask.id){
+          text = updateText
+        }
+        newTasks.push({
+          ...task,
+          text: text,
+          isEditable : false
+        })
+      })
+      
+      setTasks(newTasks)
     }
 
     return (
         <>
-          <input type="text" placeholder="add items"  value={lists}
-          onChange={(e)=> setLists(e.target.value)} />
+          <input type="text" placeholder="add items"   
+          value={inputTask.text}
+          onChange={(e)=> setInputTask({
+            "id" : Math.floor(Math.random()*1000),
+            "text" : e.target.value,
+            "isEditable" :  false
+
+          })} /> 
           <button onClick={addItem} >Add</button>
-          {items.map((item)=> (
-            <div>
-                <span>{item}</span>
-                    <button onClick={() => {
-                        let selectedarry = items.find((value)=> value === item)
-                        setItems(selectedarry)
-                        console.log(selectedarry);
-                    }} >Edit</button>
+          <h1>TO DO LIST</h1>
+          {tasks.map((task)=> (
+            <div className="container" key={task.id}>
+                {
+            task.isEditable ? 
+            <input type="text" className="input" defaultValue={task['text']} onChange={(event) => setUpdateText(event.target.value)} />
+            :
+            <p>{task['text']}</p>
+          }
+          
                 <button onClick={()=> {
-                    let listarray =   items.filter((value) => value !== item);
-                  setItems([...listarray])
+                    let listarray =   tasks.filter((value) => value !== task);
+                  setTasks([...listarray])
                 }} >Delete</button>
+                <button onClick={(event) => {
+                  if(task.isEditable){
+                    updatetask(task);
+                event.target.innerText = "save"    
+                  }else if(!task.isEditable){
+                    makeEditable(task);
+                    event.target.innerText = "edit"
+                  }
+                 }} >Edit</button>
             </div>
           ))}
         </>
     )
 }
 
-
-// import React, { useState } from "react";
-
-// export function TodoList() {
-//     const [items, setItems] = useState([]);
-//     const [lists, setList] = useState();
-
-//     const addItem = () => {
-//         setItems([...items, lists]);
-//         setList("")
-//     }
-
-//     return (
-//         <>
-//             <input type="text" value={lists} onChange={(e) => setList(e.target.value)} />
-//             <button onClick={addItem}>add</button>
-//             {items.map((item) => (
-//                 <div>
-//                     <span>{item}</span>
-//                     <button onClick={() => {
-//                         let filteredarray = items.filter((value) => value !== item);
-//                         setItems([...filteredarray])
-//                     }}
-//                     >delete</button>
-//                     <button onClick={() => {
-//                         let filteredarray = items.filter((value) => value !== item);
-//                         setItems([...filteredarray])
-//                     }}
-//                     >save</button>
-//                 </div>
-//             ))}
-//         </>
-//     )
-// }
