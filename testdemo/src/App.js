c37b9b815bdcd54769ef07a999686e19960b9609
Prabhat mishra <105411899+Prabhatm09@users.noreply.github.com>
2022-11-02 10:57:07 +0530

    code coverage

--- testdemo/src/App.js
+++ testdemo/src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { Counter } from "./counter/Counter"
+import { Greet } from "./greet/Greet"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-          <p>This is prabhat</p>
-      </header>
+    <div >
+      <Counter />
+      <Greet  name="prabhat"  />
     </div>
   );
 }
---
+++ testdemo/src/counter/Counter.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Counter.css"
+
+
+export function Counter(){
+    return (
+      <div>
+        <h1 className="Counter_title" >Hello this is counter</h1>
+      </div>
+    );
+}
