e3f48e40a53b1c24260461c4655f77ff0219b889
Prabhat <prabhatmishra1409@gmail.com>
2023-01-06 23:25:01 +0530

    prac

--- hooks/src/components/Counter.js
+++ hooks/src/components/Counter.js
@@ -1,33 +1,34 @@
-import React , { useState } from "react";
+import React, { useState } from "react";
 
-export function Counter(){
+export function Counter() {
 
     const initailValue = 0
-    const [ count , setCount] = useState(initailValue);
-    const [name , setName ] = useState("parbhat");
+    const [count, setCount] = useState(initailValue);
+    const [name, setName] = useState("parbhat");
 
     const handleName = () => {
-if(name !== "parbhat"){
-                setName(name.toUpperCase())
-            }
+        if (name !== "parbhat") {
+            setName(name.toUpperCase())
+        }
     }
-     
+
     const handleIncreament = () => {
         setCount(count + 1)
     }
-        const handledecreament = (value) => {
+    const handledecreament = (value) => {
         setCount(count + value)
     }
 
     return (
         <>
-        <p>count : {count}</p>
-        <p>My name is : {name}</p>
-        <button onClick={handleIncreament}>inc</button>
-        <button onClick={()=> handledecreament(3)}>dec</button>
-        <button onClick={()=> setCount(initailValue)}>reset</button>
-        {/* <button onClick={()=> setName(name.toUpperCase())}>Captilize</button> */}
-        <button onClick={ handleName}>Captilize</button>
+            <p>count : {count}</p>
+            <p>My name is : {name}</p>
+            <button onClick={handleIncreament}>inc</button>
+            <button onClick={() => handledecreament(3)}>dec</button>
+            <button onClick={() => setCount(initailValue)}>reset</button>
+            {/* <button onClick={()=> setName(name.toUpperCase())}>Captilize</button> */}
+            <button onClick={handleName}>Captilize</button>
         </>
     )
 }
+
